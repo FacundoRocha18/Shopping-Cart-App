357997da579bdb941b502e81c50cb2682df267e2
Facundo Rocha <facundo.rocha@outlook.com>
2022-03-22 18:07:12 -0300

    22/3 18:00 update

--- js/app.js
+++ js/app.js
@@ -1,14 +1,27 @@
+/* Getting products add buttons */
 const addToShoppingCartButtons = document.querySelectorAll('.addToCart');
 addToShoppingCartButtons.forEach((addToCartButton) => {
     addToCartButton.addEventListener('click', addToCartClicked);
 });
 
-const shoppingCartItemsContainer = document.querySelector('.item-rows-container');
+/* Buy button */
+const buyButton = document.querySelector('.comprarButton');
+buyButton.addEventListener('click', buyButtonClicked);
 
+/* Modal closing button */
+const closeModalButton = document.querySelector('.close-modal-btn');
+closeModalButton.addEventListener('click', toggleModalState);
+
+/* Alerts and modal */
 const successAlert = document.querySelector('.success-alert');
 const errorAlert = document.querySelector('.error-alert');
+const modalItemsContainer = document.querySelector('.modal-item-container');
+
+/* Shopping cart items container */
+const shoppingCartItemsContainer = document.querySelector('.item-rows-container');
 
 
+/* checks if add to cart button was pressed and asigns the item title, price and image to constants */
 function addToCartClicked(event) {
 
     const button = event.target;
@@ -21,29 +34,31 @@ function addToCartClicked(event) {
     addItemToShoppingCart(itemTitle, itemPrice, itemImage);
 }
 
+
+/* Builds and adds the selected product item to the cart container */
 function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
 
-    const shoppingCartRowElement = shoppingCartItemsContainer.querySelector('.shopping-cart-item');
-    console.log(shoppingCartRowElement);
-    const elementTitle = shoppingCartRowElement.getElementsByClassName('.shopping-cart-item-title');
-    console.log(elementTitle);
+    const elementTitle = shoppingCartItemsContainer.getElementsByClassName('shopping-cart-item-title');
 
-    for ( let i = 0; i < elementTitle.length; i++ ) {
+    for (let i = 0; i < elementTitle.length; i++) {
 
-        alert('hola');
-        if ( elementTitle[i] === itemTitle ) {
+        if (elementTitle[i].innerText === itemTitle) {
+
             let elementsQuantity = elementTitle[i].closest('.shopping-cart-item').querySelector('.shoppingCartItemQuantity');
 
-            console.log(elementsQuantity);
-            elementsQuantity.value++;
-            successAlert.classList.add('active');
-            updateShoppingCartTotal();
-            return;
+            try {
+                elementsQuantity.value++;
+                toggleSuccessAlertState();
+                setTimeout(toggleSuccessAlertState, 3000);
+                updateShoppingCartTotal();
+                return;
+            } catch (e) {
+                toggleSuccessAlertState();
+                setTimeout(toggleSuccessAlertState, 3000);
+                console.log(e);
+            }
         }
     }
-
-
-
 
     const shoppingCartRow = document.createElement('div');
     const shoppingCartContent = `
@@ -63,8 +78,7 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
                                 <div class="shopping-cart-quantity-container">
                                     <div class='shopping-cart-quantity'>
                                         <div class='quantity-down-btn'><span>-</span></div>
-                                        <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number"
-                                        value="1">
+                                        <input class="shopping-cart-quantity-input shoppingCartItemQuantity" type="number" value="1" min="1" max="101">
                                         <div class='quantity-up-btn'><span>+</span></div>
                                     </div>
                                     <div class='remove-cart-item-button-container'>
@@ -73,21 +87,46 @@ function addItemToShoppingCart(itemTitle, itemPrice, itemImage) {
                                 </div>
                             </div>
                         </div>`;
+
     shoppingCartRow.innerHTML = shoppingCartContent;
     shoppingCartItemsContainer.append(shoppingCartRow);
+    toggleSuccessAlertState();
+    setTimeout(toggleSuccessAlertState, 3000);
 
     shoppingCartRow.querySelector('.remove-item-btn').addEventListener('click', deleteShoppingCartItem);
 
     shoppingCartRow.querySelector('.shoppingCartItemQuantity').addEventListener('change', quantityChanged);
 
-    shoppingCartRow.querySelector('.quantity-down-btn').addEventListener('click', changeQuantityDown);
+    shoppingCartRow.querySelector('.quantity-down-btn').addEventListener('click', decreaseQuantity);
 
-    shoppingCartRow.querySelector('.quantity-up-btn').addEventListener('click', changeQuantityUp);
+    shoppingCartRow.querySelector('.quantity-up-btn').addEventListener('click', increaseQuantity);
 
 
     updateShoppingCartTotal();
 }
 
+/* Toggles the success alert state */
+function toggleSuccessAlertState() {
+
+    successAlert.classList.toggle('alert-inactive');
+
+}
+
+/* Toggles the error alert state */
+function toggleErrorAlertState() {
+
+    errorAlert.classList.toggle('alert-inactive');
+
+}
+
+/* Toggles the modal alert state */
+function toggleModalState() {
+
+    modalItemsContainer.classList.toggle('modal-inactive');
+
+}
+
+/* Updates the shopping cart total */
 function updateShoppingCartTotal() {
     let total = 0;
 
@@ -107,46 +146,63 @@ function updateShoppingCartTotal() {
 
         total = total + shoppingCartItemPrice * shoppingCartItemQuantity;
     });
-    
+
     shoppingCartTotal.innerHTML = `${total.toFixed(2)}`;
-    
+
 }
 
+/* Deletes the closest cart item to the pressed button */
 function deleteShoppingCartItem(event) {
     const buttonClicked = event.target;
     buttonClicked.closest('.shopping-cart-item').remove();
+    toggleSuccessAlertState();
+    setTimeout(toggleSuccessAlertState, 3000);
     updateShoppingCartTotal();
 }
 
-function changeQuantityUp(event) {
+/* Increases the input quantity */
+function increaseQuantity(event) {
     const buttonUpClicked = event.target;
     const inputWrapper = buttonUpClicked.closest('.shopping-cart-quantity');
     const input = inputWrapper.querySelector('.shoppingCartItemQuantity');
-    
-    if ( input.value == 99 ) {
-        input.value = 99;
+
+    if (input.value == 100) {
+        input.value = 100;
     } else {
+        toggleSuccessAlertState();
+        setTimeout(toggleSuccessAlertState, 3000);
         return quantityChanged(input.value++);
     }
 
 }
 
-function changeQuantityDown(event) {
+/* Deecreases the input quantity */
+function decreaseQuantity(event) {
     const buttonUpClicked = event.target;
     const inputWrapper = buttonUpClicked.closest('.shopping-cart-quantity');
     const input = inputWrapper.querySelector('.shoppingCartItemQuantity');
-    
-    if ( input.value == 1 ) {
+
+    if (input.value == 1) {
         input.value = 1;
     } else {
+        toggleSuccessAlertState();
+        setTimeout(toggleSuccessAlertState, 3000);
         return quantityChanged(input.value--);
     }
 
 }
 
+/* Checks if quantity has changed */
 function quantityChanged(input) {
-    
-  
+
+
     input.value <= 0 ? (input.value = 1) : null;
     updateShoppingCartTotal();
+}
+
+/* Checks if buy button was pressed */
+function buyButtonClicked() {
+    shoppingCartItemsContainer.innerHTML = '';
+    updateShoppingCartTotal();
+    toggleModalState(); 
 }
